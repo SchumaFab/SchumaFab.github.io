ee22cfe3612b69e613e7b8ad5184b1bd0a2ba3de
SchumaFab <46028251+SchumaFab@users.noreply.github.com>
2019-01-07 15:44:23 +0100

    Update Logic.js

--- Logic.js
+++ Logic.js
@@ -8,11 +8,36 @@ const main_inhalttext_datum = document.querySelector('.main-inhalttext-datum');
 // Einzeiler
 setInterval(() => {
     addNewDiaryEntry();
-}, 2000);
+}, 68400000);
 
 
 // Funktionen
 function addNewDiaryEntry(){
-    var inputField =  document.createElement("input");
+    let inputField =  document.createElement("input");
     inputField.setAttribute("type","text");  
-    document.querySelector(".main-inhalttext-fabian").appendChild(inputField);}
+    inputField.setAttribute("id","main-inputfeld"); 
+    let inputField1 =  document.createElement("input");
+    inputField1.setAttribute("type","text");  
+    inputField1.setAttribute("id","main-inputfeld"); 
+    let dateField = document.createElement("p")
+    dateField.setAttribute("type", "text");
+    let dateNow = getDate();
+    dateField.innerText = dateNow;
+    
+
+    document.querySelector(".main-inhalttext-fabian").appendChild(inputField);
+    document.querySelector(".main-inhaltdatum-datum").appendChild(dateField);
+    document.querySelector(".main-inhalttext-rebecca").appendChild(inputField1);
+}
+
+function getDate(){
+    var date = new Date();
+    var tag = date.getDate();
+    var monatDesJahres = date.getMonth();
+    var jahr = date.getFullYear();
+    var tagInWoche = date.getDay();
+    var wochentag = new Array("Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag");
+    var monat = new Array("Januar", "Februar", "M&auml;rz", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember");
+    var datum = tag + "." + (monatDesJahres + 1) + "." + jahr;
+    return datum;
+}
