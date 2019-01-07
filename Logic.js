25897a8368326a9f1e50d3a8ae373bf45b9a091f
SchumaFab <46028251+SchumaFab@users.noreply.github.com>
2019-01-07 15:08:39 +0100

    Update Logic.js

--- Logic.js
+++ Logic.js
@@ -1,11 +1,18 @@
 'use strict'
 
 // Variablen Deklaration
+const main_inhalttext_fabian = document.querySelector('.main-inhalttext-fabian');
+const main_inhalttext_rebecca = document.querySelector('.main-inhalttext-rebecca');
+const main_inhalttext_datum = document.querySelector('.main-inhalttext-datum');
 
 // Einzeiler
-
-
-// Funktionen Deklaration
+setInterval(() => {
+    addNewDiaryEntry();
+}, 2000);
 
 
 // Funktionen
+function addNewDiaryEntry(){
+    var inputField =  document.createElement("input");
+    inputField.setAttribute("type","text");  
+    document.querySelector(".main-inhalttext-fabian").appendChild(inputField);}
